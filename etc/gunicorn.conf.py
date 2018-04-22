43ffe689d73a24203eac3321df6b01c0159451c1
maximmigood <maksim.mikheev@gmail.com>
2018-04-22 10:24:46 +0300

    22/04/18 1

---
+++ etc/gunicorn.conf.py
@@ -0,0 +1,7 @@
+bind = "127.0.0.1:8080"
+workers = 2
+user = ""
+group = ""
+logfile = "/home/box/web/etc/gunicorn.log"
+loglevel = "info"
+proc_name = ""
--- hello.py
+++ hello.py
@@ -1,6 +1,8 @@
 #
-def simple_app(environ, start_response):
+def hello(environ, start_response):
     status = '200 OK'
     response_headers = [('Content-type','text/plain')]
     start_response(status, response_headers)
-    return ['Hello !!! world!\n']
+    #return ['Hello !!! world!\n']
+    aaa = environ["QUERY_STRING"]
+    return aaa.split("&")
