e73a52f7fb9b6d61d8298c10ca43467b52d26e81
maximmigood <maksim.mikheev@gmail.com>
2018-05-02 23:44:52 +0300

    02/05/18-3

--- etc/init.sh
+++ etc/init.sh
@@ -1,3 +1,3 @@
 sudo rm -rf /etc/nginx/sites-enabled/default
-sudo ln -sf ./nginx.conf /etc/nginx/sites-enabled/my.conf
+sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/my.conf
 sudo service nginx restart
